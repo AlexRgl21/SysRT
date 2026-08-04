7f9fa141567375356ce0016f49697fdf58d3f6b9
AlexRgl21 <brayanrgl070806@gmail.com>
2026-08-04 00:52:35 -0600

    feat: configura el servidor principal con Express y CORS

---
+++ punto-ventas-backend/src/servidor.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+require('./configuracion/base_datos');
+
+const app = express();
+const PUERTO = process.env.PUERTO_SERVIDOR || 3000;
+
+app.use(cors());
+app.use(express.json())
+
+app.get('/api', (req, res) => {
+    res.json({ mensaje: 'El backend está funcionando' });
+});
+
+app.listen(PUERTO, () => {
+    console.log(`Servidor corriendo en http://localhost:${PUERTO}`);
+});

92848ba33b92e023102e35cf4f9733effd973249
AlexRgl21 <brayanrgl070806@gmail.com>
2026-08-04 00:50:20 -0600

    feat: establece la conexion a la base de datos PostgreSQL con pg

---
+++ punto-ventas-backend/src/configuracion/base_datos.js
@@ -0,0 +1,21 @@
+const {Pool} = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    user: process.env.USUARIO_BD,
+    host: process.env.HAST_BD,
+    database: process.env.NOMBRE_BD,
+    password: process.env.PASSWORD_BD,
+    port: process.env.PUERTO_BD
+})
+
+pool.connect((err, client, release) => {
+    if (err) {
+        console.error('Error al conectar con PostgreSQL:', err.stack);
+    } else {
+        console.log('Conexión exitosa a la base de datos');
+        release();
+    }
+});
+
+module.exports = pool;

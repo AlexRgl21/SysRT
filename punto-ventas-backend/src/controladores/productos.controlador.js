b42a9c1e9e31b9f4bc297b592c15643f1db6395a
AlexRgl21 <brayanrgl070806@gmail.com>
2026-08-04 16:05:43 -0600

    feat: implementa transaccion sql para registro de productos

--- punto-ventas-backend/src/controladores/productos.controlador.js
+++ punto-ventas-backend/src/controladores/productos.controlador.js
@@ -13,6 +13,45 @@ const obtenerProductos = async(req, res) => {
     }
 };
 
+
+// REGISTRAR PRODUCTOS
+const crearProducto = async(req, res) => {
+    const {nombre, categoria_id, precio_compra, precio_venta, stock_actual, codigo } = req.body;
+
+    try {
+        await pool.query('BEGIN');
+
+        const queryProducto = `
+        INSERT INTO productos (nombre, categoria_id, precio_compra, precio_venta, stock_actual)
+        VALUES ($1, $2, $3, $4, $5) RETURNING id, nombre;
+        `;
+        const valoresProducto = [nombre, categoria_id || null, precio_compra || 0.00, precio_venta, stock_actual || 0.000];
+        const resultadoProducto = await pool.query(queryProducto, valoresProducto);
+        const nuevoProducto = resultadoProducto.rows[0];
+
+        if (codigo) {
+            const queryCodigo = `
+                INSERT INTO codigos_barras (producto_id, codigo) 
+                VALUES ($1, $2);
+            `;
+            await pool.query(queryCodigo, [nuevoProducto.id, codigo]);
+        }
+
+        await pool.query('COMMIT');
+
+        res.status(201).json({
+            mensaje: 'Producto registrado exitosamente',
+            producto: nuevoProducto
+        });
+
+    } catch (error) {
+        await pool.query('ROLLBACK');
+        console.error('Error al registrar el producto:', error);
+        res.status(500).json({ mensaje: 'Error interno al registrar el producto' });
+    }
+};
+
 module.exports = {
-    obtenerProductos
+    obtenerProductos,
+    crearProducto
 };

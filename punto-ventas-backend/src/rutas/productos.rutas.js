19861e48c6cd513a59f3bf68be2087de5b1196fd
AlexRgl21 <brayanrgl070806@gmail.com>
2026-08-05 14:15:43 -0600

    feat: agrega endpoints put y delete para la administracion del inventario

--- punto-ventas-backend/src/rutas/productos.rutas.js
+++ punto-ventas-backend/src/rutas/productos.rutas.js
@@ -1,10 +1,16 @@
 const { Router } = require('express');
-const { obtenerProductos, crearProducto } = require('../controladores/productos.controlador');
+
+const { obtenerProductos, crearProducto, actualizarProducto, eliminarProducto } = require('../controladores/productos.controlador');
 
 const enrutador = Router();
 
 enrutador.get('/productos', obtenerProductos);
 
 enrutador.post('/productos', crearProducto);
+
+enrutador.put('/productos/:id', actualizarProducto);
+
+enrutador.delete('/productos/:id', eliminarProducto);
+
 
 module.exports = enrutador;

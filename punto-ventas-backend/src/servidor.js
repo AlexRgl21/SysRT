82342a970f6826fadc806096e36f25b960120fb8
AlexRgl21 <brayanrgl070806@gmail.com>
2026-08-05 17:18:26 -0600

    fix(api): corrige orden de inicialización de rutas e integra middleware CORS

--- punto-ventas-backend/src/servidor.js
+++ punto-ventas-backend/src/servidor.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 
+
 require('./configuracion/base_datos');
+const productosRutas = require('./rutas/productos.rutas'); 
 
 const app = express();
 const PUERTO = process.env.PUERTO_SERVIDOR || 3000;
 
+
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 app.get('/api', (req, res) => {
     res.json({ mensaje: 'El backend está funcionando' });
 });
+app.use('/api', productosRutas); 
 
 app.listen(PUERTO, () => {
     console.log(`Servidor corriendo en http://localhost:${PUERTO}`);
 });
-
-
-const productosRutas = require('./rutas/productos.rutas');
-app.use('/api', productosRutas);

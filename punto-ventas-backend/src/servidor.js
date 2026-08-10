406573ebfd1251a94cfb6ec15d32be1664850a71
AlexRgl21 <brayanrgl070806@gmail.com>
2026-08-10 15:42:06 -0600

    feat(servidor): registra e integra el enrutador de ventas en la app

--- punto-ventas-backend/src/servidor.js
+++ punto-ventas-backend/src/servidor.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 require('./configuracion/base_datos');
 const productosRutas = require('./rutas/productos.rutas'); 
+const ventasRutas = require('./rutas/ventas.rutas');
 
 const app = express();
 const PUERTO = process.env.PUERTO_SERVIDOR || 3000;
@@ -16,6 +17,7 @@ app.get('/api', (req, res) => {
     res.json({ mensaje: 'El backend está funcionando' });
 });
 app.use('/api', productosRutas); 
+app.use('/api', ventasRutas);
 
 app.listen(PUERTO, () => {
     console.log(`Servidor corriendo en http://localhost:${PUERTO}`);

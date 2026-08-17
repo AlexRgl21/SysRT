cc75e8c8af2fb1236b43cf0eff0425a18f408af9
AlexRgl21 <brayanrgl070806@gmail.com>
2026-08-17 12:00:26 -0600

    feat(compras): implementar GET y renderizado dinámico del directorio.
    Se creó la funcion 'cargarDirectorio' para consultar el endpoitn /api/proveedores.
    - Se implementó la lógica para inyectar dinámicamente las filas (tr) en la tabla.

--- punto-ventas-frontend/js/compras.js
+++ punto-ventas-frontend/js/compras.js
@@ -44,4 +44,130 @@ document.addEventListener('DOMContentLoaded', () => {
     if (tabActivaInicial) {
         tabActivaInicial.click();
     }
+
+    // MODAL NUEVO PROVEEDOR
+    const modalProveedor = document.getElementById('modalProveedor');
+    const formNuevoProveedor = document.getElementById('formNuevoProveedor');
+    const btnCerrarModalProveedor = document.getElementById('btnCerrarModalProveedor');
+    const btnCancelarProveedor = document.getElementById('btnCancelarProveedor');
+
+    btnAccionPrincipal.addEventListener('click', () => {
+        const tabActiva = document.querySelector('.tab-btn.activo').getAttribute('data-target');
+
+        if (tabActiva === 'tab-directorio') {
+            modalProveedor.style.display = 'flex';
+            document.getElementById('provNombre').focus();
+        }
+    });
+
+    const cerrarModalProveedor = () => {
+        modalProveedor.style.display = 'none';
+        formNuevoProveedor.reset();
+    };
+
+    btnCerrarModalProveedor.addEventListener('click', cerrarModalProveedor);
+    btnCancelarProveedor.addEventListener('click', cerrarModalProveedor);
+
+    window.addEventListener('click', (e) => {
+        if (e.target === modalProveedor) {
+            cerrarModalProveedor();
+        }
+    });
+
+    formNuevoProveedor.addEventListener('submit', async (e) => {
+        e.preventDefault(); 
+
+        const nuevoProveedor = {
+            nombre: document.getElementById('provNombre').value.trim(),
+            telefono: document.getElementById('provTelefono').value.trim(),
+            dias_visita: document.getElementById('provDias').value.trim()
+        };
+
+        try {
+            const respuesta = await fetch('http://localhost:3000/api/proveedores', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(nuevoProveedor)
+            });
+
+            const data = await respuesta.json();
+
+            if (respuesta.ok) {
+                cerrarModalProveedor();
+                cargarDirectorio();
+                
+                Toastify({
+                    text: "¡Proveedor registrado con éxito!",
+                    duration: 3000,
+                    gravity: "top",
+                    position: "center",
+                    style: {
+                        background: "#10b981", 
+                        borderRadius: "8px"
+                    }
+                }).showToast();
+                
+            } else {
+                throw new Error(data.error || 'Error al guardar');
+            }
+
+        } catch (error) {
+            console.error('Error:', error);
+            Toastify({
+                text: "Ocurrió un error al guardar el proveedor.",
+                duration: 3000,
+                gravity: "top",
+                position: "center",
+                style: {
+                    background: "#ef4444", 
+                    borderRadius: "8px"
+                }
+            }).showToast();
+        }
+    });
 });
+
+//LOGICA DE LA TABLA DIRECTORIO
+    const bodyProveedores = document.getElementById('bodyProveedores');
+
+    const cargarDirectorio = async () => {
+        try {
+            const respuesta = await fetch('http://localhost:3000/api/proveedores');
+            const proveedores = await respuesta.json();
+
+            bodyProveedores.innerHTML = '';
+
+            if (proveedores.length === 0) {
+                bodyProveedores.innerHTML = `
+                    <tr>
+                        <td colspan="3" style="text-align: center; padding: 20px; color: #94a3b8;">No hay proveedores registrados.</td>
+                    </tr>`;
+                    return;
+            }
+
+            proveedores.forEach(prov => {
+                const tr = document.createElement('tr');
+                tr.style.borderBottom = '1px solid #f1f5f9';
+
+                tr.innerHTML = `
+                    <td style="padding: 15px; font-weight: 500; color: #1e293b;">${prov.nombre}</td>
+                    <td style="padding: 15px; color: #475569;">${prov.telefono || '-'}</td>
+                    <td style="padding: 15px; color: #475569;">${prov.dias_visita || '-'}</td>
+                `;
+
+                bodyProveedores.appendChild(tr);
+            });
+        } catch (error) {
+            console.error('Error al cargar los proveedores.', error);
+            bodyProveedores.innerHTML = `
+                <tr>
+                    <td colspan="3" style="text-align: center; padding: 20px; color: #ef4444;">Error al cargar el directorio.</td>
+                </tr>`;
+        }
+    };
+
+    cargarDirectorio();
+
+

fb22d75b0a8d71c7f6edd074120814d7f0664f87
AlexRgl21 <brayanrgl070806@gmail.com>
2026-08-21 16:48:13 -0600

    feat(rutas): exponer endpoint para consultar historial de ventas

--- punto-ventas-backend/src/rutas/ventas.rutas.js
+++ punto-ventas-backend/src/rutas/ventas.rutas.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
-const { registrarVenta } = require('../controladores/ventas.controlador');
+const { registrarVenta, obtenerVentas } = require('../controladores/ventas.controlador');
 
 const enrutador = Router();
 
 enrutador.post('/ventas', registrarVenta);
+
+enrutador.get('/ventas', obtenerVentas);
 
 module.exports = enrutador;
